a67495d05e2eb509e607cb9d09d45c4ec0e564ee
anasuss <88457250+anasuss@users.noreply.github.com>
2022-06-09 20:45:54 +0100

    Time: 1057 ms (10.86%), Space: 207.8 MB (5.17%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-   vector<vector<int>> graph_directed  ;
+unordered_map<int,unordered_set<int>>graph_directed  ;
 
 vector<vector<int>> graph_undirected ;
 
@@ -14,11 +14,10 @@ void dfs(int node){
     visited[node] = true ;
     for (int child : graph_undirected[node]){
         if (!visited[child]){
-        auto it = find(graph_directed[child].begin(),graph_directed[child].end(),node) ;
+        auto it = graph_directed[child].find(node) ;
         if (it == graph_directed[child].end()){
-            graph_directed[child].push_back(node) ;
-            it = find(graph_directed[node].begin(),graph_directed[node].end(),child) ;
-            graph_directed[node].erase(it) ;
+            graph_directed[child].insert(node) ;
+            graph_directed[node].erase(child) ;
             ++m ;
         }
         dfs(child);
@@ -26,14 +25,14 @@ void dfs(int node){
     }
 }
 int minReorder(int n, vector<vector<int>>& connections) {
-    graph_directed.resize(n) ;
+    graph_directed.clear() ;
     graph_undirected.resize(n) ;
-    visited.resize(n,false) ;
+    visited.resize(n) ;
       for (int i = 0 ; i<(int)connections.size() ; i++){
             int from , to ;
             from = connections[i][0]  ;
             to = connections[i][1] ;
-            graph_directed[from].push_back(to) ;
+            graph_directed[from].insert(to) ;
             graph_undirected[from].push_back(to) ;
             graph_undirected[to].push_back(from) ;
       }
