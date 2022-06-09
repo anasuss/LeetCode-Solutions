a5bcd06a4e27441f32455a5e4c7b0917fcc936c8
anasuss <88457250+anasuss@users.noreply.github.com>
2022-06-09 20:07:56 +0100

    Time: 518 ms (63.30%), Space: 129.1 MB (30.38%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+   vector<vector<int>> graph_directed  ;
+
+vector<vector<int>> graph_undirected ;
+
+vector<bool> visited ;
+
+int m = 0 ;
+
+void dfs(int node){
+    if (visited[node])
+        return ;
+    visited[node] = true ;
+    for (int child : graph_undirected[node]){
+        if (!visited[child]){
+        auto it = find(graph_directed[child].begin(),graph_directed[child].end(),node) ;
+        if (it == graph_directed[child].end()){
+            graph_directed[child].push_back(node) ;
+            it = find(graph_directed[node].begin(),graph_directed[node].end(),child) ;
+            graph_directed[node].erase(it) ;
+            ++m ;
+        }
+        dfs(child);
+        }
+    }
+}
+int minReorder(int n, vector<vector<int>>& connections) {
+    graph_directed.resize(n) ;
+    graph_undirected.resize(n) ;
+    visited.resize(n,false) ;
+      for (int i = 0 ; i<(int)connections.size() ; i++){
+            int from , to ;
+            from = connections[i][0]  ;
+            to = connections[i][1] ;
+            graph_directed[from].push_back(to) ;
+            graph_undirected[from].push_back(to) ;
+            graph_undirected[to].push_back(from) ;
+      }
+        m = 0 ;
+        dfs(0) ;
+    return m ;
+}
+};
