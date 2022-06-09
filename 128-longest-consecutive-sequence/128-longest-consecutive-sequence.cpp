33d1e60275c2e7bb94bfe3ca52c9f3e42f6b5b63
anasuss <88457250+anasuss@users.noreply.github.com>
2022-06-09 16:20:06 +0100

    Time: 789 ms (19.68%), Space: 52 MB (8.65%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
-unordered_map<int,vector<int> > graph ;
-
-
-unordered_map<int,int> mp ;
-
-int cnt = 0 ;
-
-void dfs(int node ){
-    ++cnt ;
-    for (int i = 0 ; i<graph[node].size() ; i++)
-        dfs(graph[node][i]);
-
-}
-int longestConsecutive(vector<int>& nums) {
-     graph.clear() ;
-     mp.clear() ;
-     for (int i = 0 ; i<nums.size() ; i++){
-         ++mp[nums[i]];
-     }
-     for (auto it = mp.begin() ; it!=mp.end() ; it++){
-        int x = it->first ;
-        if (mp.find(x+1) != mp.end()){
-            graph[x].push_back(x+1) ;
+  int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, int> u;
+        int result = 0;
+        for (const auto& x : nums)
+        {
+            u[x]++;
         }
-     }
-
-     int ans = 0 ;
-     for (auto it = mp.begin() ; it!=mp.end() ; it++){
-            int x = it->first ;
-            if (mp.find(x-1) == mp.end()){
-                cnt = 0 ;
-                dfs(x);
-                ans = max(ans,cnt) ;
+        for (const auto& x : nums)
+        {
+            if (!u[x - 1])
+            {
+                int i = 0;
+                while (u[x + i])
+                {
+                    i++;
+                }
+                result = max(result, i);
             }
-     }
-     return ans ;
-}
+        }
+        return result;
+    }
 };
