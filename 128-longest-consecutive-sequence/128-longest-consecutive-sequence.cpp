06775ca64349e99d7cea891d024a48cf1e0f7314
anasuss <88457250+anasuss@users.noreply.github.com>
2022-06-09 16:19:08 +0100

    Time: 677 ms (27.18%), Space: 102.1 MB (5.12%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,8 +1,5 @@
 class Solution {
 public:
-unordered_set<int> visited ;
-
-
 unordered_map<int,vector<int> > graph ;
 
 
@@ -11,38 +8,30 @@ unordered_map<int,int> mp ;
 int cnt = 0 ;
 
 void dfs(int node ){
-
-    if (visited.find(node) != visited.end())
-        return ;
-    visited.insert(node) ;
     ++cnt ;
-   // cout << cnt << endl ;
     for (int i = 0 ; i<graph[node].size() ; i++)
         dfs(graph[node][i]);
 
 }
 int longestConsecutive(vector<int>& nums) {
-
      graph.clear() ;
-     visited.clear() ;
-
+     mp.clear() ;
      for (int i = 0 ; i<nums.size() ; i++){
          ++mp[nums[i]];
      }
-     for (int i = 0 ; i<nums.size() ; i++){
-        int x = nums[i] ;
+     for (auto it = mp.begin() ; it!=mp.end() ; it++){
+        int x = it->first ;
         if (mp.find(x+1) != mp.end()){
             graph[x].push_back(x+1) ;
-            graph[x+1].push_back(x) ;
         }
      }
 
      int ans = 0 ;
-     for (int i = 0 ; i<nums.size() ; i++){
-            if (visited.find(nums[i]) == visited.end()){
+     for (auto it = mp.begin() ; it!=mp.end() ; it++){
+            int x = it->first ;
+            if (mp.find(x-1) == mp.end()){
                 cnt = 0 ;
-                dfs(nums[i]);
-               // cout << cnt << endl ;
+                dfs(x);
                 ans = max(ans,cnt) ;
             }
      }
