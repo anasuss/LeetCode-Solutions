4c080e1c52af6c0bf2aa00ef1af998877dc413f9
anasuss <88457250+anasuss@users.noreply.github.com>
2022-06-11 14:43:55 +0100

    Time: 222 ms (72.94%), Space: 30.6 MB (87.08%) - LeetHub

--- 752-open-the-lock/752-open-the-lock.cpp
+++ 752-open-the-lock/752-open-the-lock.cpp
@@ -3,18 +3,21 @@ public:
 int openLock(vector<string>& deadends, string target) {
     queue<string> q ;
     unordered_set<string> visited ;
+    for (string s : deadends)
+        if (s == "0000")
+            return -1 ; 
+        else 
+            visited.insert(s) ; 
     q.push("0000") ;
     visited.insert("0000") ;
-    if (find(deadends.begin(),deadends.end(),"0000") != deadends.end())
-            return -1 ;
+
     for (int level = 0 , sz = 1 ; !q.empty() ; level++ , sz = q.size()){
         while(sz--){
             string s = q.front() ;
             q.pop() ;
              if (s == target)
                 return level ; 
-             if (find(deadends.begin(),deadends.end(),s) != deadends.end())
-                continue;
+            
             string sn = s ;
             for (int j = 0 ; j<4 ; j++){
                 int n = s[j]-'0' ;
