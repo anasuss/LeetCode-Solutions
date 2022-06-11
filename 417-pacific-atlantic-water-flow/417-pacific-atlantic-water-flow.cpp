4a6a97ef99f8ec8b6f8e3c696e168d5e2925d4db
anasuss <88457250+anasuss@users.noreply.github.com>
2022-06-11 18:55:34 +0100

    Time: 54 ms (65.94%), Space: 18.6 MB (59.27%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
- int dx[4] = {0,1,0,-1} ;
+int dx[4] = {0,1,0,-1} ;
 
 int dy[4] = {1,0,-1,0} ;
 
@@ -8,60 +8,53 @@ bool isvalide(int x , int y,int n , int m){
     return x>=0 && y>=0 && x<n && y<m ;
 }
 
-bool bfs(int x , int y,vector<vector<int>>& heights){
+void bfs(vector<vector<int>>& heights , vector<vector<bool>> &visited , vector<pair<int,int>> &occ){
     queue<pair<int,int>> q ;
-    int n = (int)heights.size() ;
-    int m = (int)heights[0].size() ;
-    bool visited[n][m] ;
-    memset(visited,false,sizeof(visited)) ;
-    q.push({x,y}) ;
-    visited[x][y] = true ;
-    bool PO = false ;
-    bool PA = false ;
-    if (x == 0 || y == 0)
-        PO = true ;
-    if (x == n-1 || y == m-1)
-        PA = true ;
-    if (PO && PA )
-        return true ;
+    int n = heights.size() ;
+    int m = heights[0].size() ;
+    for (int i = 0 ; i<occ.size() ; i++){
+        q.push(occ[i]);
+        visited[occ[i].first][occ[i].second] = true ;
+    }
     while(!q.empty()){
         pair<int,int> p = q.front() ;
         q.pop() ;
-        x = p.first ;
-        y = p.second ;
+        int x = p.first ;
+        int y = p.second ;
         for (int i = 0 ; i<4 ; i++){
             int nx = x+dx[i] ;
             int ny = y+dy[i] ;
-            if (isvalide(nx,ny,n,m) && heights[nx][ny]<=heights[x][y] && !visited[nx][ny]){
+            if (isvalide(nx,ny,n,m) && heights[nx][ny]>=heights[x][y] && !visited[nx][ny]){
                 q.push({nx,ny}) ;
                 visited[nx][ny] = true ;
-                if (nx == 0 || ny == 0)
-                        PO = true ;
-                if (nx == n-1 || ny == m-1)
-                        PA = true ;
-                if (PO && PA)
-                    return true ;
             }
         }
     }
-    return false ;
+    return  ;
 }
 
 vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
     vector<vector<int>> ans ;
-    vector<int> v ;
     int n = (int)heights.size() ;
     int m = (int)heights[0].size() ;
+    vector<vector<bool>> visited1(n, vector<bool>(m,false));
+    vector<vector<bool>> visited2(n, vector<bool>(m,false));
+    vector<pair<int,int>> po ;
+    vector<pair<int,int>> ao ;
     for (int i = 0 ; i<n ; i++){
-        for (int j = 0 ; j<m ; j++){
-            if (bfs(i,j,heights)){
-                v.push_back(i); v.push_back(j) ;
-                ans.push_back(v) ;
-                v.clear() ;
-            }
-        }
+        po.push_back({i,0}) ;
+        ao.push_back({i,m-1}) ;
     }
-    return ans ;
+    for (int i = 0 ; i<m ; i++){
+        po.push_back({0,i}) ;
+        ao.push_back({n-1,i}) ;
+    }
+    bfs(heights,visited1,po) ;
+    bfs(heights,visited2,ao) ;
+    for (int i = 0 ; i<n ; i++)
+        for (int j = 0 ; j<m ; j++)
+            if (visited1[i][j] && visited2[i][j])
+                ans.push_back({i,j}) ;
+    return ans ;  
 }
-
 };
