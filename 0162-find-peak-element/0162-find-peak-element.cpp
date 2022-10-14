9bed41583410a17a2b7b0ce6c203cd87381d86d0
anasuss <88457250+anasuss@users.noreply.github.com>
2022-10-14 16:59:06 +0100

    Time: 8 ms (53.76%), Space: 8.8 MB (89.33%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -12,13 +12,12 @@ public:
         int l = 1 , r = n-1 ; 
         while(l<=r){
             int mid = l+(r-l)/2 ; 
-            if (nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+            if (nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
                 return mid ; 
-            }
-            if (nums[mid-1]>nums[mid])
-                r = mid-1 ; 
-            else 
+            if (nums[mid]<nums[mid+1])
                 l = mid+1 ; 
+            else 
+                r = mid-1; 
         }
         return 0 ; 
     }
