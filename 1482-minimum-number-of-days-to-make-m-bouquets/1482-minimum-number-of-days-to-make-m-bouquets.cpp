474e356e6b06de04ae57aa66974c210b841cbd35
anasuss <88457250+anasuss@users.noreply.github.com>
2022-10-15 18:19:43 +0100

    Time: 571 ms (5.04%), Space: 66.6 MB (12.47%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
-bool possible(vector<int>& bloomDay, int m, int k , int d){
-    int b = 0 ;
-    int nb = 0 ;
-    for (int i = 0 ; i<bloomDay.size(); i++)
-        if (bloomDay[i]<=d){
-            b++ ;
-            if (b == k)
-                ++nb , b = 0 ;
-            if (nb == m )
-                return true ;
-        }else
-            b = 0 ;
-    return false ;
-}
-
-int minDays(vector<int>& bloomDay, int m, int k){
-
-    if (m*k>bloomDay.size())
-        return -1 ;
-    int l = 1 ,  r = *max_element(bloomDay.begin(),bloomDay.end()) ;
-    int ans = -1 ;
-    while (r>=l){
-        int mid = l+(r-l)/2 ;
-        if (possible(bloomDay,m,k,mid))
-            r = mid-1 , ans = mid ;
-        else
-            l = mid+1;
+    typedef long long ll ; 
+    bool is_possible(ll maxD,vector<int>&v,int m , int k){
+        int j = 0 ;
+        int cnt = 0 ; 
+        int mx = 0 ;
+        int idxmx = 0 ; 
+        for (int i = 0 ; i<v.size() ; i++){
+            if (v[i]>mx){
+                mx = v[i] ; 
+                idxmx = i ; 
+            }
+            if (mx<=maxD){
+                ++j ; 
+                if (j == k){
+                    ++cnt ; 
+                    j = 0 ; 
+                }
+            }else{
+                j = 0 ; 
+                i = idxmx;
+                mx = 0 ; 
+                idxmx = 0 ; 
+            }
+        }
+        return cnt>=m ; 
     }
-    return ans ;
-}
-
-
-
+    int minDays(vector<int>& bloomday, int m, int k) {
+        ll n = bloomday.size() ;
+        ll x = ll(m)*(ll)k ; 
+        if (x>n)
+            return -1 ; 
+        ll l = 1 , r = 1e9 ; 
+        while(l<=r){
+            ll mid = l+(r-l)/2 ; 
+            if (is_possible(mid,bloomday,m,k))
+                r = mid-1 ; 
+            else 
+                l = mid+1 ; 
+        }
+        return (int)r+1 ; 
+    }
 };
