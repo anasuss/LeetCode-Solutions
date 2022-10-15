a9a03a59e8bbca56567a6784a84ed265419de296
anasuss <88457250+anasuss@users.noreply.github.com>
2022-10-15 19:25:38 +0100

    Time: 152 ms (6.30%), Space: 22.4 MB (22.86%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -14,13 +14,13 @@ public:
     }
     int smallestDivisor(vector<int>& nums, int threshold) {
         int l = 1 , r = *max_element(nums.begin(),nums.end()) ; 
-        while(l<=r){
+        while(l<r){
             int mid = l+(r-l)/2 ; 
             if (is_possible(mid,nums,threshold))
-                r = mid-1 ; 
+                r = mid ; 
             else 
                 l = mid+1 ; 
         }
-        return r+1 ; 
+        return l ; 
     }
 };
