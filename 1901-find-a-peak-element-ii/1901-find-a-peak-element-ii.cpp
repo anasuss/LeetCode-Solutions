b81fa71914d32565ad123522df6175f14d5bb290
anasuss <88457250+anasuss@users.noreply.github.com>
2022-10-15 17:26:55 +0100

    Time: 361 ms (49.27%), Space: 46.3 MB (43.65%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -5,30 +5,35 @@ public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int n = mat.size() ; 
         int m = mat[0].size() ; 
-        for (int i = 0 ; i<n ; i++){
-            int l = 0 , r = m-1 ; 
-            while(l<=r){
-                int mid = l+(r-l)/2 ;
-                int val = mat[i][mid] ;
-                bool check = true ; 
-                for (int j = 0 ; j<4 ; j++){
-                    int x = i+dx[j] ; 
-                    int y = mid+dy[j] ; 
-                    if (x<0 || y<0 || x>=n || y>=m)
-                        continue ; 
-                    if (val<mat[x][y]){
-                        check = false ; 
-                        break ; 
-                    }
+        int l = 0 , r = n-1 ; 
+        while(l<=r){
+            int mid = l+(r-l)/2 ; 
+            int mx = mat[mid][0] , idmx = 0 ; 
+            for (int j = 1 ; j<m ; j++){
+                if (mat[mid][j]>mx){
+                    mx = mat[mid][j] ; 
+                    idmx = j ; 
                 }
-                if (check)
-                    return {i,mid} ; 
-                if (mid+1<m && val<mat[i][mid+1])
-                    l = mid+1 ; 
-                else 
-                    r = mid-1 ; 
             }
+            bool check = true ; 
+            for (int j = 0 ; j<4 ; j++){
+                int x = mid+dx[j] ; 
+                int y = idmx+dy[j] ; 
+                if (x<0 || y<0 || x>=n || y>=m)
+                    continue ; 
+                if (mat[x][y]>mx){
+                    check = false ;
+                    break ; 
+                }
+            }
+            if (check){
+                return {mid,idmx} ; 
+            }
+            if (mat[mid][idmx]<mat[mid+1][idmx])
+                l = mid+1 ; 
+            else 
+                r = mid-1 ; 
         }
-        return{1,1} ; 
+        return{-1,-1} ; 
     }
 };
