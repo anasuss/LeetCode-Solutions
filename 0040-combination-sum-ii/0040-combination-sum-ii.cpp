5336dd100e4c74b5bcdc5de4a69f498fbeefb67b
anasuss <88457250+anasuss@users.noreply.github.com>
2022-10-21 00:56:42 +0100

    Time: 9 ms (64.86%), Space: 10.8 MB (37.42%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> v ; 
+    vector<vector<int>> ans ;
+    bitset<101> bs ; 
+    void work(vector<int>&candidates , int target , int idx = 0 ){
+        if (target == 0){    
+            ans.push_back(v) ; 
+            return ; 
+        }
+        for (int i = idx ; i<candidates.size() ; i++){
+            if (i>idx && candidates[i] == candidates[i-1]) continue ; 
+            if (candidates[i]>target) break ; 
+            v.push_back(candidates[i]);
+            work(candidates,target-candidates[i],i+1) ; 
+            v.pop_back() ; 
+        }
+      
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        work(candidates,target) ; 
+        return ans; 
+    }
+};
