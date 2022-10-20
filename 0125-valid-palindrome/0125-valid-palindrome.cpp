7e05210e76e550cbcdcc773f9bbd944471b9ce83
anasuss <88457250+anasuss@users.noreply.github.com>
2022-10-20 17:59:25 +0100

    Time: 28 ms (5.96%), Space: 7.7 MB (27.68%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,25 +1,7 @@
 class Solution {
 public:
-    string sn = "" ; 
-    bool work(int i = 0){
-        int j = sn.size()-1-i ;
-        if (sn.size()%2 == 0)
-            if (i+1 == j)
-                if (sn[i] == sn[j])
-                    return true ; 
-                else 
-                    return false ; 
-        if (sn[i] != sn[j] || i>j)
-            return false ; 
-        if (i == j && sn[i] == sn[j])
-            return true ;
-        if (sn[i] == sn[j] && work(i+1) )
-            return true ; 
-        return false ; 
-    }
-    bool isPalindrome(string s){
-        if (s.size() == 0)
-            return true ; 
+    bool isPalindrome(string s){ 
+        string sn = "" ; 
         for (int i = 0 ; i<s.size() ; i++){
             s[i] = tolower(s[i]) ; 
             if (isalpha(s[i]) || (s[i]>='0' && s[i]<='9'))
@@ -27,6 +9,11 @@ public:
         }
         if (sn.size() == 0)
             return true ; 
-        return work() ; 
+        int i = 0 , j = sn.size()-1 ; 
+        while(i<j){
+            if (sn[i] != sn[j]) return false ;
+            ++i , --j ; 
+        }
+        return true ; 
     }
 };
