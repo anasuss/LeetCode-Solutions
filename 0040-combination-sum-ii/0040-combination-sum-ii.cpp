9bfaf2d2f663b5a8d118e20f02f0a16d61ae9a98
anasuss <88457250+anasuss@users.noreply.github.com>
2022-10-21 22:15:08 +0100

    Time: 22 ms (20.04%), Space: 14.4 MB (15.76%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -2,20 +2,28 @@ class Solution {
 public:
     vector<int> v ; 
     vector<vector<int>> ans ;
+    unordered_set<int> si ; 
     bitset<101> bs ; 
-    void work(vector<int>&candidates , int target , int idx = 0 ){
-        if (target == 0){    
+    void work(vector<int>&candidates , int target , int i = 0 , int x = -1){
+        if (target == 0){
             ans.push_back(v) ; 
             return ; 
         }
-        for (int i = idx ; i<candidates.size() ; i++){
-            if (i>idx && candidates[i] == candidates[i-1]) continue ; 
-            if (candidates[i]>target) break ; 
-            v.push_back(candidates[i]);
-            work(candidates,target-candidates[i],i+1) ; 
-            v.pop_back() ; 
+        if (i == candidates.size())
+            return ; 
+        if (x != -1 && candidates[i] == x){
+            work(candidates,target,i+1,x) ;
+            return ; 
         }
-      
+        if (candidates[i]<=target && !bs[i]){
+            v.push_back(candidates[i]) ; 
+            bs[i] = true ; 
+            work(candidates,target-candidates[i],i+1,x) ;
+            bs[i] = false ;
+            x = v.back() ; 
+            v.pop_back() ; 
+        } 
+        work(candidates,target,i+1,x) ; 
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
