92fda2fa071a95a8bd1786b9e8b9fd26dd30e7a3
anasuss <88457250+anasuss@users.noreply.github.com>
2022-10-21 22:20:04 +0100

    Time: 14 ms (41.65%), Space: 14.5 MB (15.15%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -3,26 +3,29 @@ public:
     vector<int> v ; 
     vector<vector<int>> ans ;
     bitset<101> bs ; 
-    void work(vector<int>&candidates , int target , int i = 0 , int x = -1){
+    int x ; 
+    void work(vector<int>&candidates , int target , int i = 0 ){
         if (target == 0){
             ans.push_back(v) ; 
             return ; 
         }
         if (i == candidates.size())
             return ; 
+        if (candidates[i]>target)
+            return ; 
         if (x != -1 && candidates[i] == x){
-            work(candidates,target,i+1,x) ;
+            work(candidates,target,i+1) ;
             return ; 
         }
         if (candidates[i]<=target && !bs[i]){
             v.push_back(candidates[i]) ; 
             bs[i] = true ; 
-            work(candidates,target-candidates[i],i+1,x) ;
+            work(candidates,target-candidates[i],i+1) ;
             bs[i] = false ;
             x = v.back() ; 
             v.pop_back() ; 
         } 
-        work(candidates,target,i+1,x) ; 
+        work(candidates,target,i+1) ;
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
