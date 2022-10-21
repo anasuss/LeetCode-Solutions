8d24a8778ac33437218dfaf5bada5dec69da5f79
anasuss <88457250+anasuss@users.noreply.github.com>
2022-10-21 23:44:28 +0100

    Time: 6 ms (26.53%), Space: 6.3 MB (83.18%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -3,17 +3,17 @@ public:
     int numbers[9] = {1,2,3,4,5,6,7,8,9} ; 
     vector<vector<int>> ans ; 
     vector<int> v ; 
-    void work(int k , int n , int i = 0 , int used = 0){
-        if (n == 0 && used == k){
+    void work(int k , int n , int i = 0 ){
+        if (n == 0 && k == 0){
             ans.push_back(v) ; 
             return ; 
         }
-        if (i == 9 || numbers[i]>n || used>k)
+        if (i == 9 || numbers[i]>n || k<0)
             return ; 
         v.push_back(numbers[i]) ; 
-        work(k,n-numbers[i],i+1,used+1) ; 
+        work(k-1,n-numbers[i],i+1) ; 
         v.pop_back() ; 
-        work(k,n,i+1,used) ; 
+        work(k,n,i+1) ; 
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         work(k,n) ; 
