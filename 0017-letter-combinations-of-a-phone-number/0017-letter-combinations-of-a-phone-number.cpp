5fd94a9a416fc98017865f602ac3bff613693c58
anasuss <88457250+anasuss@users.noreply.github.com>
2022-10-23 12:38:31 +0100

    Time: 8 ms (5.74%), Space: 6.4 MB (81.33%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     vector<vector<char>> map = {{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
     vector<string> ans ;
-    void work(string& dg , int i = 0 , string s = ""){
+    void work(string& dg , string& s ,int i = 0){
         if (i == dg.size()){
             ans.push_back(s) ; 
             return ; 
         }
-        int x = dg[i]-'0'-2 ;
+        int x = dg[i]-'2' ;
         for (char c : map[x]){
-            work(dg,i+1,s+c) ; 
+            s.push_back(c) ; 
+            work(dg,s,i+1) ; 
+            s.pop_back() ; 
         }
     }
     vector<string> letterCombinations(string digits) {
         if (digits == ""){
             return {} ; 
         }
-        work(digits) ; 
+        string s = "" ; 
+        work(digits,s) ; 
         return ans ; 
     }
 };
