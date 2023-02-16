ef430e1f25838cefb3bed7832a26f959d7a3f116
anasuss <88457250+anasuss@users.noreply.github.com>
2023-02-16 22:45:43 +0100

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -5,32 +5,53 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
+    private:
+        vector<int> size , parent ; 
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[])
-    {
+	int UParent(int x){
+	    if (parent[x] == x)
+	        return x ; 
+	   return parent[x] = UParent(parent[x]) ; 
+	}
+	void Union(int x , int y){
+	    int px = UParent(x) ; 
+	    int py = UParent(y) ; 
+	    if (size[px]<size[py]){
+	        parent[px] = py ; 
+	        size[py]+=size[px] ; 
+	    }else{
+	        parent[py] = px ; 
+	        size[px]+=size[py] ; 
+	    }
+	}
+    int spanningTree(int V, vector<vector<int>> adj[]){
         // code here
-        int ans = 0 ; 
-        priority_queue<pair<int,int>> pq ; 
-        vector<bool> visited(V,false) ; 
-        pq.push({1,0}) ; 
-        while(!pq.empty()){
-            int node = pq.top().second ; 
-            int w = -pq.top().first ; 
-            pq.pop() ; 
-            if (visited[node])
-                continue ; 
-            visited[node] = true ; 
-            if (w != -1)
-                ans+=w ; 
-            for (auto pair : adj[node]){
-                int child = pair[0] ; 
-                int we = pair[1] ; 
-                if(!visited[child])
-                    pq.push({-we,child}) ; 
+        size.resize(V,1) ; 
+        parent.resize(V) ;
+        for (int i = 0 ; i<V ; i++)
+            parent[i] = i ; 
+        vector<pair<int,pair<int,int>>> v ; 
+        for (int i = 0 ; i<V ; i++){
+            for (auto it : adj[i]){
+                int x = it[0] ; 
+                int w = it[1] ; 
+                int y = i ; 
+                v.push_back({w,{x,y}}) ; 
             }
         }
-        return ans; 
+        sort(v.begin(),v.end()) ; 
+        int ans = 0 ; 
+        for (int i = 0 ; i<v.size() ; i++){
+            int w = v[i].first ; 
+            int x = v[i].second.first ; 
+            int y = v[i].second.second ; 
+            if (UParent(x) != UParent(y)){
+                ans+=w ; 
+                Union(x,y) ; 
+            }
+        }
+        return ans ; 
     }
 };
 
